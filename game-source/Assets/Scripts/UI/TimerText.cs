d206a00bd74016ed1a28b21ffb4083e9a814904f
trokair <wandos1@yahoo.com>
2019-01-26 00:57:41 -0600

    added TimerText

---
+++ game-source/Assets/Scripts/UI/TimerText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+/**
+ * Shows the current accumulated time
+ */
+public class TimerText : MonoBehaviour
+{
+    [Inject]
+    private Timer _timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Text>().text = _timer.GetTime().ToString();
+    }
+}
