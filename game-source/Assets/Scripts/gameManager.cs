db9e39f35da0fd22046621e462b4f610bd8baf58
Ridley506 <47047195+Ridley506@users.noreply.github.com>
2019-01-26 01:10:18 -0800

    Added Menu Controlls to the Game Manager

--- game-source/Assets/Scripts/gameManager.cs
+++ game-source/Assets/Scripts/gameManager.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 
 public class gameManager : MonoBehaviour
 {
+    //Set the starting point for the karma system
     public static int KarmaPoints = 0;
 
     private void Start()
     {
+        //If the current level in the main menu, reset the karma points.
         DontDestroyOnLoad(this.gameObject);
         Scene CurrentScene = SceneManager.GetActiveScene();
         if(CurrentScene.name == "Main Menu")
         {
             KarmaPoints = 0;
         }
+    }
+
+    //Load the first level
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    //Quit Game
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
