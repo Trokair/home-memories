5299eb1d92724740bc18b1c828c6c104f108f10f
trokair <wandos1@yahoo.com>
2019-01-26 13:49:57 -0600

    fixed installer script to be able to transfer over scenes

--- game-source/Assets/Scripts/Installers/GameInstaller.cs
+++ game-source/Assets/Scripts/Installers/GameInstaller.cs
@@ -9,7 +9,9 @@ public class GameInstaller : MonoInstaller
 {
     public override void InstallBindings()
     {
+        Timer timer = new Timer(400f);
         Container.Bind<Controls>().AsSingle().NonLazy();
-        Container.Bind<Timer>().FromInstance(new Timer(400f)).AsSingle().NonLazy();
+        Container.Bind<Timer>().FromInstance(timer).AsSingle().NonLazy();
+        Object.DontDestroyOnLoad(this);
     }
 }
--- game-source/Assets/Scripts/UI/DontDestroyCanvas.cs
+++ game-source/Assets/Scripts/UI/DontDestroyCanvas.cs
@@ -1,21 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Zenject;
 
-public class DarknessTile : MonoBehaviour
+public class DontDestroyCanvas : MonoBehaviour
 {
-    [Inject]
-    private Timer _timer;
     // Start is called before the first frame update
     void Start()
     {
-        Object.DontDestroyOnLoad(this);
+         Object.DontDestroyOnLoad(this);
     }
 
     // Update is called once per frame
     void Update()
     {
-        _timer.Update(this);
+        
     }
 }
