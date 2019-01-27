bc449aeb52dd246c254a0ef4d9953785f96329b6
trokair <wandos1@yahoo.com>
2019-01-27 08:05:37 -0600

    halfway through hooking up scenes and transitions

---
+++ game-source/Assets/Scripts/TransitionToLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+/**
+ * Currently, can't use the door script to transtion because it uses the E key which is the same as everything else
+ */
+public class TransitionToLevel : MonoBehaviour
+{
+    public string scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
